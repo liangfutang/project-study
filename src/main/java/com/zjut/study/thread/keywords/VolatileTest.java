613c24fa5af195f9814398a59d0833e410377365
liangfutang <liangfu.tang@ecarx.com.cn>
2019-11-28 20:23:20 +0800

    新加测试依赖

---
+++ src/main/java/com/zjut/study/thread/keywords/VolatileTest.java
@@ -0,0 +1,11 @@
+package com.zjut.study.thread.keywords;
+
+import org.junit.jupiter.api.Test;
+
+public class VolatileTest {
+
+    @Test
+    void noVolatile() {
+        System.out.println("测试");
+    }
+}
