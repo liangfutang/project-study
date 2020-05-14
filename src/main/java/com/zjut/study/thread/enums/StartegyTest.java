2eaa980f4cd2ad5a6c297268c1ef02a0af0d02b0
liangfutang <liangfu.tang@ecarx.com.cn>
2020-05-14 10:20:56 +0800

    测试枚举

---
+++ src/main/java/com/zjut/study/thread/enums/StartegyTest.java
@@ -0,0 +1,12 @@
+package com.zjut.study.thread.enums;
+
+import org.junit.jupiter.api.Test;
+
+public class StartegyTest {
+
+    @Test
+    public void testStartegy() {
+        Startegy.FAST.run();
+    }
+
+}
