f94e2ac59f82bb88bc2a892d224bff0a9eba0240
liangfutang <liangfu.tang@ecarx.com.cn>
2020-09-14 19:43:48 +0800

    修改Spring査包

---
+++ study-spring/src/main/java/com/zjut/study/spring/di/B.java
@@ -0,0 +1,7 @@
+package com.zjut.study.spring.di;
+
+public class B {
+    public B() {
+        System.out.println("输出B");
+    }
+}
--- study-spring/src/main/java/com/zjut/study/spring/di/Client.java
+++ study-spring/src/main/java/com/zjut/study/spring/di/Client.java
@@ -2,14 +2,19 @@ package com.zjut.study.spring.di;
 
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
+@ComponentScan("com.zjut.study.spring")
+@ImportResource("classpath:spring.xml")
 public class Client {
 
     @Test
     public void testDi1() {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
-        Object a = annotationConfigApplicationContext.getBean("a");
-        System.out.println("输出:" + a);
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Client.class);
+        A a = (A) annotationConfigApplicationContext.getBean("a");
+        B b = (B) annotationConfigApplicationContext.getBean("b");
+        System.out.println("输出:" + a +" " + b);
     }
 
 }
