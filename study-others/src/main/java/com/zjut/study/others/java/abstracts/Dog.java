749cad8fcdc110f23ece9c03fac6bd47866bb6b1
liangfutang <liangfu.tang@ecarx.com.cn>
2020-09-25 18:25:27 +0800

    抽象类中的静态方法可以不用实例化使用

---
+++ study-others/src/main/java/com/zjut/study/others/java/abstracts/Dog.java
@@ -0,0 +1,11 @@
+package com.zjut.study.others.java.abstracts;
+
+public abstract class Dog {
+    public static void eat() {
+        System.out.println("狗到天边吃大便");
+    }
+
+    public static void main(String[] args) {
+        Dog.eat();
+    }
+}
