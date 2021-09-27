1ee5f482bcef211e70af846d8d653b429ef6f72d
唐良甫 <tangliangfucom@sina.com>
2021-09-27 15:14:59 +0800

    nio的多线程处理

--- study-netty/src/main/java/com/zjut/study/netty/nio/c3/Client.java
+++ study-netty/src/main/java/com/zjut/study/netty/nio/c3/Client.java
@@ -5,13 +5,17 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
 
     @Test
     public void test01() throws IOException {
         SocketChannel channel = SocketChannel.open();
-        channel.connect(new InetSocketAddress("localhost", 8080));
+        channel.connect(new InetSocketAddress("localhost", 8081));
+
+        int hi = channel.write(StandardCharsets.UTF_8.encode("hi"));
+        System.out.println("写出结果:"+ hi);
 
         System.out.println("waiting...");
 //        channel.close();  //正常关闭连接
