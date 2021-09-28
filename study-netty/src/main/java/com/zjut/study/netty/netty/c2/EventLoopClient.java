a309bdc7a56f1ad9fca6c903e64be8e24a6d4c50
唐良甫 <tangliangfucom@sina.com>
2021-09-28 11:21:52 +0800

    优化客户端

--- study-netty/src/main/java/com/zjut/study/netty/netty/c2/EventLoopClient.java
+++ study-netty/src/main/java/com/zjut/study/netty/netty/c2/EventLoopClient.java
@@ -1,7 +1,7 @@
 package com.zjut.study.netty.netty.c2;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -18,7 +18,7 @@ public class EventLoopClient {
 
     @Test
     public void test01() throws InterruptedException, IOException {
-        ChannelFuture channelFuture = new Bootstrap()
+        Channel channel = new Bootstrap()
                 .group(new NioEventLoopGroup())
                 .channel(NioSocketChannel.class)
                 .handler(new ChannelInitializer<NioSocketChannel>() {
@@ -29,11 +29,15 @@ public class EventLoopClient {
                 })
                 // 1. 连接到服务器
                 // 异步非阻塞, main 发起了调用，真正执行 connect 是 nio 线程
-                .connect(new InetSocketAddress("localhost", 8080));
+                .connect(new InetSocketAddress("localhost", 8080))
+                // 2.1 使用 sync 方法同步处理结果
+                // 阻塞住当前线程，直到nio线程连接建立完毕
+                .sync()
+                .channel();
 
-        // 2.1 使用 sync 方法同步处理结果
-        // 阻塞住当前线程，直到nio线程连接建立完毕
-        channelFuture.sync().channel().writeAndFlush("hello world !");
+        channel.writeAndFlush("hello world !");
+        Thread.sleep(1000);
+        channel.writeAndFlush("hi");
 
         System.in.read();
     }
