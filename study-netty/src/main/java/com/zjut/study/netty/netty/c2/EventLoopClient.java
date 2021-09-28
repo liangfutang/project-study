420cfc9f421979a19d16afa3c9123b9fb7741d6d
唐良甫 <tangliangfucom@sina.com>
2021-09-28 17:57:07 +0800

    添加客户端--优雅关闭group

--- study-netty/src/main/java/com/zjut/study/netty/netty/c2/EventLoopClient.java
+++ study-netty/src/main/java/com/zjut/study/netty/netty/c2/EventLoopClient.java
@@ -1,13 +1,11 @@
 package com.zjut.study.netty.netty.c2;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -105,7 +103,7 @@ public class EventLoopClient {
                 String line = scanner.nextLine();
                 if ("Q".equals(line)) {
                     log.info("结束连接");
-                    channel.close(); // 异步操作
+                    channel.close(); // 异步操作，此处后面的代码不一定是在该行代码后面运行
                     scanner.close();
                     LockSupport.unpark(currentThread);
                     break;
@@ -117,6 +115,52 @@ public class EventLoopClient {
         }).start();
 
         LockSupport.park();
+    }
+
+    @Test
+    public void test04() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        ChannelFuture channelFuture = new Bootstrap()
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel nsc) throws Exception {
+                        nsc.pipeline().addLast(new LoggingHandler());
+                        nsc.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080));
+
+        Channel channel = channelFuture.sync().channel();
+        log.info("当前连接上的channel:{}", channel);
+
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String line = scanner.nextLine();
+                if ("Q".equals(line)) {
+                    channel.close();  // 开始异步的断开操作
+                    log.info("即将开始和服务端断开...");
+                }
+                channel.writeAndFlush(line);
+            }
+        }).start();
+
+        // 获取 CloseFuture 对象， 1) 同步处理关闭， 2) 异步处理关闭
+        ChannelFuture closeFuture = channel.closeFuture();
+
+        // 方式一.
+        closeFuture.sync(); // 处理关闭之后的操作
+        log.info("和服务端断开连接");
+        group.shutdownGracefully();  // 优雅的关闭group
+        log.info("关闭完成");
+
+        // 方式二. 此处需要想办法暂停主线程(main方法或使用Park等)
+//        closeFuture.addListener((ChannelFutureListener) future -> {
+//            log.info("开始断开操作");
+//            group.shutdownGracefully();
+//        });
     }
 
 }
