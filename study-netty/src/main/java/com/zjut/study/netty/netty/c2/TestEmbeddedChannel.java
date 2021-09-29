7ef796024ddeb75631b6d967e19f4ac6531cfb28
唐良甫 <tangliangfucom@sina.com>
2021-09-29 11:47:48 +0800

    EmbeddedChannel 工具的简单使用

---
+++ study-netty/src/main/java/com/zjut/study/netty/netty/c2/TestEmbeddedChannel.java
@@ -0,0 +1,61 @@
+package com.zjut.study.netty.netty.c2;
+
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.embedded.EmbeddedChannel;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * netty提供的对 pipeline 的测试类
+ */
+@Slf4j
+public class TestEmbeddedChannel {
+
+    @Test
+    public void test01() {
+        // 入栈
+        ChannelInboundHandlerAdapter h1 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.info("1");
+                super.channelRead(ctx, msg);
+            }
+        };
+        ChannelInboundHandlerAdapter h2 = new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                log.info("2");
+                super.channelRead(ctx, msg);
+            }
+        };
+
+        // 出栈
+        ChannelOutboundHandlerAdapter h3 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.info("3");
+                super.write(ctx, msg, promise);
+            }
+        };
+        ChannelOutboundHandlerAdapter h4 = new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                log.info("4");
+                super.write(ctx, msg, promise);
+            }
+        };
+
+
+        EmbeddedChannel channel = new EmbeddedChannel(h1, h2, h3, h4);
+        // 模拟入栈,不能模拟出栈
+        log.info("开始模拟入栈操作");
+        channel.writeInbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("hello".getBytes()));
+        // 模拟出栈操作
+        log.info("开始模拟出栈操作");
+        channel.writeOutbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("world".getBytes()));
+    }
+}
