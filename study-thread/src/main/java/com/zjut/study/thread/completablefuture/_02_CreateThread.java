6b767f66a4a9b8be3685aad1b7fe1c85d1719061
liangfutang <tangliangfucom@sina.com>
2022-04-08 10:12:35 +0800

    1. spring循环依赖笔记

--- study-thread/src/main/java/com/zjut/study/thread/completablefuture/_02_CreateThread.java
+++ study-thread/src/main/java/com/zjut/study/thread/completablefuture/_02_CreateThread.java
@@ -1,4 +1,32 @@
 package com.zjut.study.thread.completablefuture;
 
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author jack
+ * 创建线程测试
+ */
 public class _02_CreateThread {
+
+    /**
+     * @see Thread.State
+     */
+    @Test
+    public void threadStateTest() throws InterruptedException {
+        Thread thread = new Thread();
+        System.out.println("1-" + thread.getState());
+        thread.start();
+        System.out.println("2-" + thread.getState());
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("3-" + thread.getState());
+    }
+
+    @Test
+    public void createThreadRun() {
+        Thread thread = new Thread(() -> System.out.println("我是子线程:" + Thread.currentThread().getName()));
+        thread.start();
+        System.out.println("主线程结束:" + Thread.currentThread().getName());
+    }
 }
