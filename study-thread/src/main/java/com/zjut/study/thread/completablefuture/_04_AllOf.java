1b6a8bcabd4c50d5f12be194bbe7556e037b6034
liangfutang <tangliangfucom@sina.com>
2022-04-11 10:05:40 +0800

    1. CompletableFuture的allof测试

---
+++ study-thread/src/main/java/com/zjut/study/thread/completablefuture/_04_AllOf.java
@@ -0,0 +1,48 @@
+package com.zjut.study.thread.completablefuture;
+
+import com.zjut.study.common.junit.CommonJunitFilter;
+import com.zjut.study.common.utils.SmallThreadTool;
+import com.zjut.study.thread.completablefuture.entity.Dish;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
+/**
+ *
+ */
+public class _04_AllOf extends CommonJunitFilter {
+
+    @Test
+    public void ofAll() {
+        SmallThreadTool.printTimeAndThread("小白和小伙伴们 进餐厅点菜");
+        long startTime = System.currentTimeMillis();
+//        // 点菜
+//        List<Dish> dishList = new ArrayList<>();
+//        for (int i=0; i<=10; i++) {
+//            Dish dish = new Dish("菜" + i, 1);
+//            dishList.add(dish);
+//        }
+//        // 做菜
+//        List<CompletableFuture> cfList = new ArrayList<>();
+//        for (Dish dish : dishList) {
+//            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> dish.make());
+//            cfList.add(future);
+//        }
+//        // 等待所有任务执行完毕
+//        CompletableFuture.allOf(cfList.toArray(new CompletableFuture[cfList.size()])).join();
+
+        // 上述简单写法
+        CompletableFuture[] array = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> new Dish("菜" + i, 1))
+                .map(dish -> CompletableFuture.runAsync(() -> dish.make()))
+                .toArray(size -> new CompletableFuture[size]);
+
+        CompletableFuture.allOf(array).join();
+
+        SmallThreadTool.printTimeAndThread("菜都做好了，上桌，总共花费:" + (System.currentTimeMillis()-startTime));
+    }
+
+}
---
+++ study-thread/src/main/java/com/zjut/study/thread/completablefuture/entity/Dish.java
@@ -0,0 +1,51 @@
+package com.zjut.study.thread.completablefuture.entity;
+
+import com.zjut.study.common.utils.SmallThreadTool;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 菜
+ */
+public class Dish {
+    // 菜名
+    private String name;
+    // 制作时长 (秒)
+    private Integer productionTime;
+
+    public Dish(String name, Integer productionTime) {
+        this.name = name;
+        this.productionTime = productionTime;
+    }
+
+    // 做菜
+    public void make() {
+        SmallThreadTool.sleep(TimeUnit.SECONDS.toMillis(this.productionTime));
+        SmallThreadTool.printTimeAndThread(this.name + " 制作完毕，来吃我吧");
+    }
+
+    // 做菜
+    public void makeUseCPU() {
+        SmallThreadTool.printTimeAndThread(this.name + " 制作完毕，来吃我吧" + compute());
+    }
+
+    /**
+     * 用来模拟 1秒钟的耗时操作
+     * 如果你的电脑比较强，可以增大循环次数，否则，需要减少循环次数
+     */
+    private static long compute() {
+        long startTime = System.currentTimeMillis();
+        long result = 0;
+        // 只是用来模拟耗时操作，没有任何意义
+        for (int i = 0; i < Integer.MAX_VALUE / 3; i++) {
+            result += i * i % 3;
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(compute());
+    }
+
+
+}
