b2c6de56245b0130d1173ddb6a356b12ea72437e
liangfutang <tangliangfucom@sina.com>
2022-04-14 17:38:31 +0800

    1. 为springboot添加一个钩子

--- study-springboot/src/main/java/com/zjut/study/boot/StudyApplication.java
+++ study-springboot/src/main/java/com/zjut/study/boot/StudyApplication.java
@@ -7,5 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudyApplication.class, args);
+
+        // 添加一个钩子程序，在jvm最终关闭之前执行最后的动作
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->{
+            System.out.println("最终还是钩子抗下了所有");
+        }));
     }
 }
