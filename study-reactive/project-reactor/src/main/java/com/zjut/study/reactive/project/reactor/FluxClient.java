ab4e532ab5948f57c9a96a2248a238179e827797
tlf <tanglf@kalman-navigation.com>
2023-01-11 16:23:52 +0800

    1. 添加响应式编程简单案例

--- study-reactive/project-reactor/src/main/java/com/zjut/study/reactive/project/reactor/FluxClient.java
+++ study-reactive/project-reactor/src/main/java/com/zjut/study/reactive/project/reactor/FluxClient.java
@@ -10,4 +10,29 @@ public class FluxClient extends CommonJunitFilter {
     public void just() {
         Flux.just(1, 2, 3).subscribe(System.out::println);
     }
+
+    @Test
+    public void generate() {
+        Flux.generate(() -> 0, (i,sink) -> {
+            sink.next("2*" + i + "=" + 2*i);
+            if (9==i) {
+                sink.complete();
+            }
+            return ++i;
+        }).subscribe(System.out::println);
+    }
+
+    /**
+     * 对输入的字符串，按照顺序输出字母
+     * 输入: hello guys i am bole welcome to beijing school jdk quick fox prizev
+     * 输出: abc.....xyz
+     */
+    @Test
+    public void fromArray() {
+        Flux.fromArray("hello guys i am bole welcome to beijing school jdk quick fox prizev".split(" "))
+                .flatMap(i -> Flux.fromArray(i.split("")))
+                .distinct()
+                .sort()
+                .subscribe(System.out::print);
+    }
 }
