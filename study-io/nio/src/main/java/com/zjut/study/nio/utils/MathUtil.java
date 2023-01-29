5f9d162e277c63334d4e272d27029f6e781b635b
liangfutang <tangliangfucom@sina.com>
2023-01-29 23:29:39 +0800

    1. 添加nio模块

---
+++ study-io/nio/src/main/java/com/zjut/study/nio/utils/MathUtil.java
@@ -0,0 +1,35 @@
+package com.zjut.study.nio.utils;
+
+/**
+ * ByteBufferUtil中使用到的了netty的相关工具，这里暂时用来替换，后面重写
+ * TODO
+ * @author jack
+ */
+public final class MathUtil {
+
+    private MathUtil() {
+    }
+
+    public static int findNextPositivePowerOfTwo(int value) {
+        assert value > -2147483648 && value < 1073741824;
+
+        return 1 << 32 - Integer.numberOfLeadingZeros(value - 1);
+    }
+
+    public static int safeFindNextPositivePowerOfTwo(int value) {
+        return value <= 0 ? 1 : (value >= 1073741824 ? 1073741824 : findNextPositivePowerOfTwo(value));
+    }
+
+    public static boolean isOutOfBounds(int index, int length, int capacity) {
+        return (index | length | index + length | capacity - (index + length)) < 0;
+    }
+
+    public static int compare(int x, int y) {
+        return x < y ? -1 : (x > y ? 1 : 0);
+    }
+
+    public static int compare(long x, long y) {
+        return x < y ? -1 : (x > y ? 1 : 0);
+    }
+
+}
---
+++ study-io/nio/src/main/java/com/zjut/study/nio/utils/StringUtil.java
@@ -0,0 +1,48 @@
+package com.zjut.study.nio.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+/**
+ * ByteBufferUtil中使用到的了netty的相关工具，这里暂时用来替换，后面重写
+ * TODO
+ * @author jack
+ */
+@Slf4j
+public class StringUtil {
+    public static final String NEWLINE = StringUtil.get("line.separator", "\n");
+    private static final String[] BYTE2HEX_PAD = new String[256];
+
+    public static String get(final String key, String def) {
+        if (key == null) {
+            throw new NullPointerException("key");
+        } else if (key.isEmpty()) {
+            throw new IllegalArgumentException("key must not be empty.");
+        } else {
+            String value = null;
+
+            try {
+                if (System.getSecurityManager() == null) {
+                    value = System.getProperty(key);
+                } else {
+                    value = (String) AccessController.doPrivileged(new PrivilegedAction<String>() {
+                        @Override
+                        public String run() {
+                            return System.getProperty(key);
+                        }
+                    });
+                }
+            } catch (SecurityException var4) {
+                log.warn("Unable to retrieve a system property '{}'; default values will be used.", key, var4);
+            }
+
+            return value == null ? def : value;
+        }
+    }
+
+    public static String byteToHexStringPadded(int value) {
+        return BYTE2HEX_PAD[value & 255];
+    }
+}
