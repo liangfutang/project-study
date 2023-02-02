d5ba9947166290833701f95080c5eb3f120201c8
tlf <tanglf@kalman-navigation.com>
2023-02-02 11:33:24 +0800

    1. 文件夹复制

--- study-io/nio/src/main/java/com/zjut/study/nio/components/FileClient.java
+++ study-io/nio/src/main/java/com/zjut/study/nio/components/FileClient.java
@@ -1,5 +1,6 @@
 package com.zjut.study.nio.components;
 
+import cn.hutool.core.date.StopWatch;
 import com.zjut.study.common.junit.CommonJunitFilter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -7,6 +8,8 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * File及Channel相关
@@ -31,5 +34,30 @@ public class FileClient extends CommonJunitFilter {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void fileCopy() throws IOException {
+        StopWatch watch = new StopWatch();
+        watch.start();
+        String source = "C:\\Users\\tlf\\Desktop\\code";
+        String target = "C:\\Users\\tlf\\Desktop\\codeaaa";
+        Files.walk(Paths.get(source)).forEach(path -> {
+            try {
+                String targetName = path.toString().replace(source, target);
+                // 如果被复制的是文件夹，创建文件夹
+                if (Files.isDirectory(path)) {
+                    Files.createDirectory(Paths.get(targetName));
+                }
+                // 普通文件
+                else if (Files.isRegularFile(path)) {
+                    Files.copy(path, Paths.get(targetName));
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        watch.stop();
+        System.out.println("耗时:" + watch.getTotalTimeMillis());
     }
 }
