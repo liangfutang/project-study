9e1a19a96c047e3ce9ff7257583411b60c651848
liangfutang <tanglf@kalman-navigation.com>
2023-08-22 21:14:49 +0800

    1. 服务端向客户端写大量数据

---
+++ study-io/nio/src/main/java/com/zjut/study/nio/server/write/WriteClient.java
@@ -0,0 +1,23 @@
+package com.zjut.study.nio.server.write;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class WriteClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel channel = SocketChannel.open();
+        channel.connect(new InetSocketAddress("localhost", 8080));
+
+        // 计算接受到的字符数
+        int count = 0;
+        while (true) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024);
+            int read = channel.read(buffer);
+            System.out.println(read);
+            count += read;
+            buffer.clear();
+        }
+    }
+}
---
+++ study-io/nio/src/main/java/com/zjut/study/nio/server/write/WriteServer.java
@@ -0,0 +1,62 @@
+package com.zjut.study.nio.server.write;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+
+/**
+ * nio写操作
+ */
+public class WriteServer {
+    public static void main(String[] args) throws IOException {
+        Selector selector = Selector.open();
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        ssc.bind(new InetSocketAddress(8080));
+
+        while (true) {
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+                    SelectionKey scKey = sc.register(selector, SelectionKey.OP_READ);
+                    // 写大量的数据
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 5000000; i++) {
+                        sb.append("a");
+                    }
+                    ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    // 如果没写完则挂载到sc上
+                    if (buffer.hasRemaining()) {
+                        scKey.interestOps(scKey.interestOps() | SelectionKey.OP_WRITE);
+                        scKey.attach(buffer);
+                    }
+                } else if (key.isWritable()) {
+                    ByteBuffer attachment = (ByteBuffer) key.attachment();
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    int write = channel.write(attachment);
+                    System.out.println(write);
+
+                    if (!attachment.hasRemaining()) {
+                        key.attach(null);
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);
+                    }
+                }
+            }
+        }
+    }
+}
