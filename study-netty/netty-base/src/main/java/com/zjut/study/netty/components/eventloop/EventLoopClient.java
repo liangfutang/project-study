2cb0b9e4eda02baff713fc1ac27fd434548e410f
liangfutang <tanglf@kalman-navigation.com>
2023-09-02 23:27:18 +0800

    1. EventLoop服务端和客户端示例

--- study-netty/netty-base/src/main/java/com/zjut/study/netty/components/eventloop/EventLoopClient.java
+++ study-netty/netty-base/src/main/java/com/zjut/study/netty/components/eventloop/EventLoopClient.java
@@ -2,8 +2,11 @@ package com.zjut.study.netty.components.eventloop;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 import lombok.extern.slf4j.Slf4j;
@@ -22,7 +25,7 @@ public class EventLoopClient {
      * 使用 sync 方式阻塞 给服务端发送消息
      */
     @Test
-    public void client01() throws InterruptedException, IOException {
+    public void write01() throws InterruptedException, IOException {
         Channel channel = new Bootstrap()
                 .group(new NioEventLoopGroup())
                 .channel(NioSocketChannel.class)
@@ -35,7 +38,7 @@ public class EventLoopClient {
                 })
                 // 连接到服务器
                 // 异步非阻塞, main 发起了调用，真正执行 connect 是 nio 线程
-                .connect(new InetSocketAddress(8080))
+                .connect(new InetSocketAddress("localhost", 8080))
                 // 使用 sync 方法同步处理结果
                 // 阻塞住当前线程，直到nio线程连接建立完毕
                 .sync()
@@ -44,6 +47,34 @@ public class EventLoopClient {
         channel.writeAndFlush("hello world !");
         Thread.sleep(3000);
         channel.writeAndFlush("hi !");
+
+        System.in.read();
+    }
+
+    /**
+     * 使用监听回调方式向服务端写数据
+     */
+    @Test
+    public void write02() throws IOException {
+        ChannelFuture future = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel channel) throws Exception {
+                        channel.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                .connect(new InetSocketAddress("localhost", 8080));
+
+        future.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                Channel channel = channelFuture.channel();
+                log.info("连接上服务器:{}", channel);
+                channel.writeAndFlush("hello world!");
+            }
+        });
 
         System.in.read();
     }
