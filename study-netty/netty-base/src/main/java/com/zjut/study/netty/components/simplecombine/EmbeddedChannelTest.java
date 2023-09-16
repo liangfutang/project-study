f0f1cf2a27c25a6bcf701349d17cdc3eaf3a1aae
liangfutang <tanglf@kalman-navigation.com>
2023-09-16 19:34:25 +0800

    1. netty的pipeline的测试模拟工具类

--- study-netty/netty-base/src/main/java/com/zjut/study/netty/components/simplecombine/EmbeddedChannelTest.java
+++ study-netty/netty-base/src/main/java/com/zjut/study/netty/components/simplecombine/EmbeddedChannelTest.java
@@ -1,4 +1,4 @@
-package com.zjut.study.netty.netty.c2;
+package com.zjut.study.netty.components.simplecombine;
 
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
@@ -13,11 +13,11 @@ import org.junit.Test;
  * netty提供的对 pipeline 的测试类
  */
 @Slf4j
-public class TestEmbeddedChannel {
+public class EmbeddedChannelTest {
 
     @Test
     public void test01() {
-        // 入栈
+        // 入站
         ChannelInboundHandlerAdapter h1 = new ChannelInboundHandlerAdapter() {
             @Override
             public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
@@ -33,7 +33,7 @@ public class TestEmbeddedChannel {
             }
         };
 
-        // 出栈
+        // 出战
         ChannelOutboundHandlerAdapter h3 = new ChannelOutboundHandlerAdapter() {
             @Override
             public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
@@ -49,13 +49,12 @@ public class TestEmbeddedChannel {
             }
         };
 
-
         EmbeddedChannel channel = new EmbeddedChannel(h1, h2, h3, h4);
         // 模拟入栈,不能模拟出栈
         log.info("开始模拟入栈操作");
         channel.writeInbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("hello".getBytes()));
         // 模拟出栈操作
         log.info("开始模拟出栈操作");
-        channel.writeOutbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("world".getBytes()));
+        channel.writeInbound(ByteBufAllocator.DEFAULT.buffer().writeBytes("world".getBytes()));
     }
 }
