943049e5123cbfabb811ad3c7e4607854f191a28
liangfutang <tanglf@kalman-navigation.com>
2023-09-27 16:59:08 +0800

    1. netty接受redis协议消息

--- study-netty/netty-base/src/main/java/com/zjut/study/netty/protocol/RedisProtocol.java
+++ study-netty/netty-base/src/main/java/com/zjut/study/netty/protocol/RedisProtocol.java
@@ -8,6 +8,7 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
@@ -46,6 +47,7 @@ public class RedisProtocol {
                         protected void initChannel(NioSocketChannel channel) throws Exception {
                             channel.pipeline().addLast(new LoggingHandler());
                             channel.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                // 会在连接 channel 建立成功后，会触发 active 事件
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
                                     ByteBuf redisRequest = ctx.alloc().buffer();
@@ -77,6 +79,89 @@ public class RedisProtocol {
                     .connect(new InetSocketAddress("localhost", 6379))
                     .sync();
 
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+
+    @Test
+    public void test02() {
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        byte[] LINE = {13, 10};
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(worker);
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new LoggingHandler());
+                    ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                        // 会在连接 channel 建立成功后，会触发 active 事件
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                            // 向redis写数据
+                            set(ctx);
+                            // 从redis读数据
+                            get(ctx);
+                        }
+
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                            ByteBuf buf = (ByteBuf) msg;
+                            System.out.println(buf.toString(Charset.defaultCharset()));
+                        }
+
+                        /**
+                         * 使用set指令往redis写数据
+                         * @param ctx
+                         */
+                        private void set(ChannelHandlerContext ctx) {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            buf.writeBytes("*3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("set".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("aaa".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("bbb".getBytes());
+                            buf.writeBytes(LINE);
+                            ctx.writeAndFlush(buf);
+                        }
+
+                        /**
+                         * 使用get指令从redis读数据
+                         * @param ctx
+                         */
+                        private void get(ChannelHandlerContext ctx) {
+                            ByteBuf buf = ctx.alloc().buffer();
+                            buf.writeBytes("*2".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("get".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("$3".getBytes());
+                            buf.writeBytes(LINE);
+                            buf.writeBytes("aaa".getBytes());
+                            buf.writeBytes(LINE);
+                            ctx.writeAndFlush(buf);
+                        }
+                    });
+                }
+            });
+
+            ChannelFuture channelFuture = bootstrap.connect("localhost", 6379).sync();
             channelFuture.channel().closeFuture().sync();
         } catch (Exception e) {
             e.printStackTrace();
