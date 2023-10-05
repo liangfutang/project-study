4f36d3a196a40409aaf51ab1379fca7dd3f339d7
liangfutang <tanglf@kalman-navigation.com>
2023-10-05 18:33:56 +0800

    1. 使用固定长度解决黏包半包问题客户端

--- study-netty/netty-base/src/main/java/com/zjut/study/netty/components/pack/fixedlength/Client.java
+++ study-netty/netty-base/src/main/java/com/zjut/study/netty/components/pack/fixedlength/Client.java
@@ -1,7 +1,78 @@
 package com.zjut.study.netty.components.pack.fixedlength;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * 使用固定长度解决黏包半包问题客户端
  */
 public class Client {
+
+    public static void main(String[] args) {
+        send();
+        System.out.println("finish");
+    }
+
+    private static void send() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ChannelFuture channelFuture = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new LoggingHandler());
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                    ByteBuf buffer = ctx.alloc().buffer();
+                                    char x = '0';
+                                    Random random = new Random();
+                                    for (int i=0; i<10; i++) {
+                                        byte[] bytes = fill10Bytes(x, random.nextInt(10) + 1);
+                                        x++;
+                                        buffer.writeBytes(bytes);
+                                    }
+                                    ctx.writeAndFlush(buffer);  // 向服务端发送 10*10个byte
+                                }
+                            });
+                        }
+                    })
+                    .connect("localhost", 8080).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    /**
+     * 对不足位的补齐操作
+     * @param c
+     * @param len
+     * @return
+     */
+    public static byte[] fill10Bytes(char c, int len) {
+        byte[] bytes = new byte[10];
+        Arrays.fill(bytes, (byte) '_');
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) c;
+        }
+        System.out.println(new String(bytes));
+        return bytes;
+    }
 }
--- study-netty/netty-base/src/main/java/com/zjut/study/netty/components/pack/fixedlength/Server.java
+++ study-netty/netty-base/src/main/java/com/zjut/study/netty/components/pack/fixedlength/Server.java
@@ -1,7 +1,49 @@
 package com.zjut.study.netty.components.pack.fixedlength;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.logging.LoggingHandler;
+
 /**
  * 使用 FixedLengthFrameDecoder 固定长度解析器解决黏包半包问题
  */
 public class Server {
+    public static void main(String[] args) {
+        start();
+    }
+
+    private static void start() {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        try {
+            ChannelFuture channelFuture = new ServerBootstrap()
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(5, 5, 5))
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            // 使用固定长度的解析器接收，每次接收固定长度解析
+                            ch.pipeline().addLast(new FixedLengthFrameDecoder(10));
+                            ch.pipeline().addLast(new LoggingHandler());
+                        }
+                    })
+                    .bind(8080).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
 }
