2bf7f8dfca99c8b1490c28c2e06603bb161fb4d8
liangfutang <tanglf@kalman-navigation.com>
2023-10-14 18:41:28 +0800

    1. es整合spring boot

---
+++ study-search/elasticsearch/README.md
@@ -0,0 +1,9 @@
+# 核心概念
+es中重要概念类比mysql中的概念如下：   
+索引(index)    --> 表  
+映射(mapping)  --> 表结构  
+文档(document) --> 数据  
+
+
+
+ORM easy-es
---
+++ study-search/elasticsearch/es-boot/src/main/java/com/zjut/study/es/boot/EsBootApplication.java
@@ -0,0 +1,12 @@
+package com.zjut.study.es.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsBootApplication.class, args);
+    }
+}
