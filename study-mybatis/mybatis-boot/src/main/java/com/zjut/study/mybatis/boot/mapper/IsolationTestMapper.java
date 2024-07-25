9cf9b254c7d434921d38da2f6036daefd2c5bbc7
liangfutang <tanglf@kalman-navigation.com>
2024-07-25 10:41:30 +0800

    1. 优化mybatis-boot模块

--- study-mybatis/mybatis-boot/src/main/java/com/zjut/study/mybatis/boot/mapper/IsolationTestMapper.java
+++ study-mybatis/mybatis-boot/src/main/java/com/zjut/study/mybatis/boot/mapper/IsolationTestMapper.java
@@ -1,21 +1,15 @@
 package com.zjut.study.mybatis.boot.mapper;
 
 import com.zjut.study.mybatis.boot.entity.IsolationTestDO;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 
 /**
  * @author Jack
  */
 //@Mapper
-//    @CacheNamespace
 public interface IsolationTestMapper {
 
-    Object selectById(Integer id);
+    IsolationTestDO selectById(Integer id);
 
 //    @Select({"select id, name, age from isolation_test where name = #{name} "})
 //    @Results({
