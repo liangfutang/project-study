89150b397e936037fb3d154ea19a4da809f1d8eb
liangfutang <tanglf@kalman-navigation.com>
2024-08-23 12:19:12 +0800

    1. 初始化一个简单的swing gui程序

---
+++ study-gui/README.md
@@ -0,0 +1,15 @@
+# 打包可执行文件
++ 方法一：Launch4j + JRE 一键打包
+
++ 方法二：Inno Setup + Launch4j，做个“高大上”的安装包
+  + 主要步骤：
+    1. 使用Launch4j生成exe文件
+    2. 使用Inno Setup打包可执行文件和环境
+  + 参考文件：
+    https://blog.51cto.com/u_16099355/7869587
+    
++ 方法三：graalvm的AOT
+
+
+# 参考项目地址
+[Swing9patch](https://github.com/JackJiang2011/Swing9patch?tab=readme-ov-file)
--- study-gui/gui-swing/src/main/java/com/zjut/study/Main.java
+++ study-gui/gui-swing/src/main/java/com/zjut/study/Main.java
@@ -1,7 +1,14 @@
 package com.zjut.study;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("title");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("hello world");
+        frame.getContentPane().add(label);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
